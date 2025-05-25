e67f72b2cf0bd3dc12c77e39c8bf3cea573be69c
OmarADev <omar2942004@gmail.com>
2025-05-25 14:07:32 +0200

    Create test_headinglogic.py

---
+++ tests/test_headinglogic.py
@@ -0,0 +1,7 @@
+from navigation.headinglogic import decide_movement
+
+def test_heading_logic():
+    assert decide_movement(0, 5) == "go_straight"
+    assert decide_movement(0, 90) == "turn_right"
+    assert decide_movement(0, 270) == "turn_left"
+    assert decide_movement(350, 10) == "go_straight"
