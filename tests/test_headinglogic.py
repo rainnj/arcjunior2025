eda4be61fc48fe1525d8c7ea04e3f40a7571811e
OmarADev <omar2942004@gmail.com>
2025-05-25 14:24:22 +0200

    Update test_headinglogic.py

--- tests/test_headinglogic.py
+++ tests/test_headinglogic.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from navigation.headinglogic import decide_movement
 
 def test_heading_logic():
