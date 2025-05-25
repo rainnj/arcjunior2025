d4d64b13110e31484cb157a65a36e9884df2fbe6
OmarADev <omar2942004@gmail.com>
2025-05-25 14:23:51 +0200

    Update test_controller_simulated.py

--- tests/test_controller_simulated.py
+++ tests/test_controller_simulated.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from controller import run_simulation, load_gps_waypoints
 
 def test_controller_simulation():
