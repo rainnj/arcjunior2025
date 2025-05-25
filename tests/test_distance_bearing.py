6280249ab21ff76d5a10af8a293aba15ca9f0975
OmarADev <omar2942004@gmail.com>
2025-05-25 14:07:03 +0200

    Create test_distance_bearing.py

---
+++ tests/test_distance_bearing.py
@@ -0,0 +1,8 @@
+from navigation.distance_bearing import haversine, calculate_bearing
+
+def test_haversine():
+    assert round(haversine(52.2296756, 21.0122287, 52.406374, 16.9251681)) == 278458
+
+def test_bearing():
+    bearing = calculate_bearing(52.2296756, 21.0122287, 52.406374, 16.9251681)
+    assert 250 <= bearing <= 260  # Allow margin due to precision
